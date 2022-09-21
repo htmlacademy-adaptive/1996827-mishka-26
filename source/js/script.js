c037f448529f44a6f8d9e34e21d99b902d902d5b
kos <ustyugovki@bk.ru>
2022-09-21 16:17:55 +0300

    d2 b10 b30

--- source/js/script.js
+++ source/js/script.js
@@ -2,18 +2,26 @@
 let navMains = document.querySelectorAll('.main-nav');
 let navToggle = document.querySelector('.navigation__button-toggle');
 
+for (let navMain of navMains) {
+  navMain.classList.remove('navigation--opened');
+}
+
+// const mainnav = document.querySelectorAll('.mainnav');
+// for (let i = 0; i < mainnav.length; i++) {
+//   mainnav[i].classList.remove('navigation--opened');
+// }
+
 navToggle.addEventListener('click', function () {
 
   for (let navMain of navMains) {
-
-    navMain.classList.remove('navigation--nojs');
-
     if (navMain.classList.contains('navigation--closed')) {
-      navMain.classList.remove('navigation--closed');
       navMain.classList.add('navigation--opened');
-    } else {
-      navMain.classList.add('navigation--closed');
+      navMain.classList.remove('navigation--nojs');
+      navMain.classList.remove('navigation--closed');
+    }
+    else {
       navMain.classList.remove('navigation--opened');
+      navMain.classList.add('navigation--closed');
     }
   }
 });
